403166f3fdb9bc8427e015f246cc15f598e7f4f2
Katja <karnashauskaite@gmail.com~>
2023-03-14 14:40:25 +0100

    create first 20 cards

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -19,7 +21,20 @@ const url = "https://rickandmortyapi.com/api/character/";
 async function fetchCharacters() {
   const response = await fetch(url);
   const data = await response.json();
-
   console.log(data);
+
+  data.results.forEach((element) => {
+    let src = element.image;
+    let name = element.name;
+    let status = element.status;
+    let type = element.type;
+    let occurrences = element.episode.length;
+
+    console.log(element);
+    const card = createCharacterCard(src, name, status, type, occurrences);
+
+    cardContainer.append(card);
+  });
 }
+
 fetchCharacters();
