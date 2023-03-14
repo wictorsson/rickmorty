6b16e72401225299272e87118e61b063d1c229a1
Katja <karnashauskaite@gmail.com~>
2023-03-14 12:44:26 +0100

    fetch request

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ const maxPage = 1;
 const page = 1;
 const searchQuery = "";
 // comment
+
+const url = "https://rickandmortyapi.com/api/character/";
+
+async function fetchCharacters() {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data);
+  console.log(data.results[0].name);
+}
+fetchCharacters();
